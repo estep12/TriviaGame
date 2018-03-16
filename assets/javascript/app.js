02354f4707519377fd809b0c4f5e39b630026f4c
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-16 18:04:32 -0500

    Updated question array with objects that contain questions, choices and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,55 @@
 
 $(document).ready(function() {
 
-    var timeRemaining;
-    var questionArray = [
-        "Who was the first French president of the French Third Republic?", 
-        "What is the largest animal to have ever existed?", 
-        "What is the more common name the Thylacine?", 
-        "Who won the men's singles at the 1987 French Open?", 
-        "How long ago was the Cambrian Explosion?",
-        "Who was the 21st President of the United States?",
-        "What film won the Oscar for Best Picture in 2013?",
-        "The four chemical bases of DNA are Adenine, Cytosine, Guanine and what?",
+    var timeRemaining = 30;
+    var questionArray = [{
+        question: "Who was the first French president of the French Third Republic?",
+        options: ["Adolphe Thiers", "Napoleon Bonaparte", "Emmanuel Macron", "Charles De Gaulle"],
+        answer: 0
+        },{ 
+        question: "What is the largest animal to have ever existed?",
+        options: ["T.Rex", "African Elephant", "Blue Whale", "None of the above"],
+        answer: 2
+        },{
+        question: "What is the more common name the Thylacine?",
+        options: ["Tazmanian Devil", "Tazmanian Tiger", "African Elephant", "Blue Whale"],
+        answer: 1
+        },{
+        question: "Who won the men's singles at the 1987 French Open?",
+        options: ["Ivan Lendl", "John McEnroe", "Roger Federer", "Andy Murray"],
+        answer: 0
+        },{ 
+        question: "How long ago was the Cambrian Explosion?",
+        options: ["256 million years ago", "451 million years ago", "541 million years ago", "1 billion years ago"],
+        answer: 2
+        },{
+        question: "Who was the 21st President of the United States?",
+        options: ["Chester A. Arthur", "Abraham Lincoln", "Grover Cleveland", "Calvin Coolidge"],
+        answer: 0
+        },{
+        question: "What film won the Oscar for Best Picture in 2013?",
+        options: ["Hurt Locker", "Space Jam", "The Shape of Water", "Argo"],
+        answer: 3
+        },{
+        question: "The four chemical bases of DNA are Adenine, Cytosine, Guanine and what?",
+        options: ["Thymine", "Aniline", "Uracil", "Hydrogen"],
+        answer: 0
+        }
     ];
 
-    var answerArray = [
-        "Adolphe Thiers", 
-        "Blue Whale", 
-        "Tazmanian Tiger", 
-        "Ivan Lendl", 
-        "541 million years" ,
-        "Chester A. Arthur",
-        "Argo",
-        "Thymine"
-    ];
+    // var answerArray = [
+    //     "Adolphe Thiers", 
+    //     "Blue Whale", 
+    //     "Tazmanian Tiger", 
+    //     "Ivan Lendl", 
+    //     "541 million years" ,
+    //     "Chester A. Arthur",
+    //     "Argo",
+    //     "Thymine"
+    // ];
 
    
-console.log(questionArray[3])
+console.log(questionArray)
 
 
 
