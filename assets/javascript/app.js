31ca9e4f02530b3b6304e3068af90534870c772b
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-15 21:51:25 -0500

    Added questions and answers into questionArray and answerArray

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,9 +2,28 @@
 $(document).ready(function() {
 
     var timeRemaining;
-    var questionArray = [];
-    var answerArray = [];
+    var questionArray = [
+        "Who was the first French president of the French Third Republic?", 
+        "What is the largest animal to have ever existed?", 
+        "What is the more common name the Thylacine?", 
+        "Who won the men's singles at the 1987 French Open?", 
+        "How long ago was the Cambrian Explosion?",
+        "Who was the 21st President of the United States?",
+        "What film won the Oscar for Best Picture in 2013?",
+        "The four chemical bases of DNA are Adenine, Cytosine, Guanine and what?",
+    ];
     
+    var answerArray = [
+        "Adolphe Thiers", 
+        "Blue Whale", 
+        "Tazmanian Tiger", 
+        "Ivan Lendl", 
+        "541 million years" ,
+        "Chester A. Arthur",
+        "Argo",
+        "Thymine"
+    ];
+
 
 
 
