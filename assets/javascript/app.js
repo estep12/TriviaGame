b02fe4fd515b6bd3b966ca42d5fa8940595e30b0
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-03-19 12:34:02 -0500

    Added function to log and store correct and incorrect answers. Display correct and incorrect answers at end

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,49 @@
 
 
-
-    var timeRemaining = 30;
+    var questionPos = 0;
+    var timeRemaining = 50;
     var correct = 0;
     var incorrect = 0;
     var userChoice;
     var incorrectAnswers = 0;
     var correctAnswers = 0;
+    var choiceA;
+    var choiceB;
+    var choiceC;
+    var choiceD;
 
     var questionArray = [{
         question: "Who was the first French president of the French Third Republic?",
         options: ["Adolphe Thiers", "Napoleon Bonaparte", "Emmanuel Macron", "Charles De Gaulle"],
-        answer: 0
+        answer: "A"
         },{ 
         question: "What is the largest animal to have ever existed?",
         options: ["T.Rex", "African Elephant", "Blue Whale", "None of the above"],
-        answer: 2
+        answer: "C"
         },{
         question: "What is the more common name the Thylacine?",
         options: ["Tazmanian Devil", "Tazmanian Tiger", "African Elephant", "Blue Whale"],
-        answer: 1
+        answer: "B"
         },{
         question: "Who won the men's singles at the 1987 French Open?",
         options: ["Ivan Lendl", "John McEnroe", "Roger Federer", "Andy Murray"],
-        answer: 0
+        answer: "A"
         },{ 
         question: "How long ago was the Cambrian Explosion?",
         options: ["256 million years ago", "451 million years ago", "541 million years ago", "1 billion years ago"],
-        answer: 2
+        answer: "C"
         },{
         question: "Who was the 21st President of the United States?",
         options: ["Chester A. Arthur", "Abraham Lincoln", "Grover Cleveland", "Calvin Coolidge"],
-        answer: 0
+        answer: "A"
         },{
         question: "What film won the Oscar for Best Picture in 2013?",
         options: ["Hurt Locker", "Space Jam", "The Shape of Water", "Argo"],
-        answer: 3
+        answer: "D"
         },{
         question: "The four chemical bases of DNA are Adenine, Cytosine, Guanine and what?",
         options: ["Thymine", "Aniline", "Uracil", "Hydrogen"],
-        answer: 0
+        answer: "A"
         }
     ];
 
@@ -55,78 +59,73 @@ $("#start").on("click", function(){
     counter = setInterval(timer, 1000);
     $(".time").text("Time Remaining: " + timeRemaining + " secs");
     displayTrivia();
+    checkAnswer();
     }); 
 
     // console.log(timeRemaining)
 
 function displayTrivia() {
+    if(questionPos === questionArray.length){
+    $(".time").hide();
+    $("#q1").hide();
+    $("#q2").hide();
+    $("#q3").hide();
+    $("#q4").hide();
+    $("#submit").hide();
+    $(".questions").text("Correct Answers: " + correctAnswers + "Incorrect Answers: " + incorrectAnswers)
+    }
     for (let i = 0; i < questionArray.length; i++){
-        console.log(questionArray[i].question);
-        questionQuestion = questionArray[i].question.toString();
-        $("<p>").attr("id", questionQuestion).text(questionQuestion).appendTo($(".questions"));
-        questionAnswer = questionArray[i].options;
-        var buttonOne = $("<button>").addClass("answerA").attr("data-id", i).text(questionAnswer[0]).appendTo($(".questions"));
-        var buttonTwo = $("<button>").addClass("answerA").attr("data-id", i).text(questionAnswer[1]).appendTo($(".questions"));
-        var buttonThree = $("<button>").addClass("answerA").attr("data-id", i).text(questionAnswer[2]).appendTo($(".questions"));
-        var buttonFour = $("<button>").addClass("answerA").attr("data-id", i).text(questionAnswer[3]).appendTo($(".questions"));
+        console.log(questionArray[questionPos].question);
+        questionQuestion = questionArray[questionPos].question.toString();
+        questionAnswer = questionArray[questionPos].options;
+        answerAnswer = questionArray[questionPos].answer;
 
-    
-        $(".answerA").on("click",  function() {
-            userChoice = $(this).data("id");
-            answerAnswer = questionArray[0].answer;
-            if(userChoice === answerAnswer) {
+        choiceA = questionAnswer[0];
+        choiceB = questionAnswer[1];
+        choiceC = questionAnswer[2];
+        choiceD = questionAnswer[3];
+
+
+        $(".questions").html("<h3>" + questionQuestion + "</h3>")
+        $("#q1").html("<input type='radio' name = 'choices' value='A'> " + choiceA + "<br>");
+        $("#q2").html("<input type='radio' name = 'choices' value='B'> " + choiceB + "<br>");
+        $("#q3").html("<input type='radio' name = 'choices' value='C'> " + choiceC + "<br>");
+        $("#q4").html("<input type='radio' name = 'choices' value='D'> " + choiceD + "<br>");
+        $("#submit").html("<input type='submit'>")
+
+       
+        
+    }
+    };
+
+function checkAnswer(){
+    $("#submit").on("click", function() {
+    choices = document.getElementsByName("choices");
+    for (j = 0; j < choices.length; j++)
+        if(choices[j].checked) {
+            choice = choices[j].value;
+        }
+        console.log(choice);
+        if(choice === answerAnswer) {
+                // return;
                 correctAnswers++;
+                questionPos++;
+                displayTrivia();
                 console.log("Correct Answers: " + correctAnswers);
-            } else  (userChoice != answerAnswer);{
+                
+            } else {
+                // return;
                 incorrectAnswers++;
+                questionPos++;
+                displayTrivia(); 
                 console.log("Incorrect Answers: " + incorrectAnswers);
+                
             }
-            return;
-        });
-
-        // $(buttonTwo).on("click",  function() {
-        //     userChoice = $(this).data("id");
-        //     answerAnswer = questionArray[i].answer;
-        //     if(userChoice === answerAnswer) {
-        //         correctAnswers++;
-        //         console.log("Correct Answers: " + correctAnswers);
-        //     } else  (userChoice != answerAnswer);{
-        //         incorrectAnswers++;
-        //         console.log("Incorrect Answers: " + incorrectAnswers);
-        //     }
-        //     return;
-        // });
-
-        // $(buttonThree).on("click",  function() {
-        //     userChoice = $(this).data("id");
-        //     answerAnswer = questionArray[i].answer[i];
-        //     if(userChoice === answerAnswer) {
-        //         correctAnswers++;
-        //         console.log("Correct Answers: " + correctAnswers);
-        //     } else  (userChoice != answerAnswer);{
-        //         incorrectAnswers++;
-        //         console.log("Incorrect Answers: " + incorrectAnswers);
-        //     }
-        //     return;
-        // });
-
-        // $(buttonFour).on("click",  function() {
-        //     userChoice = $(this).data("id");
-        //     answerAnswer = questionArray[i].answer;
-        //     if(userChoice === answerAnswer) {
-        //         correctAnswers++;
-        //         console.log("Correct Answers: " + correctAnswers);
-        //     } else  (userChoice != answerAnswer);{
-        //         incorrectAnswers++;
-        //         console.log("Incorrect Answers: " + incorrectAnswers);
-        //     }
-        //     return;
-        // });
-    }
-    }
-
-
-
+           
+          
+});
+}   
+  
 
 function timer(){
 timeRemaining--;
